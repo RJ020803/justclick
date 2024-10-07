46c2c47b2bfcac77e58d550a3ebc2ab1d034a6f4
RJ020803 <142171076+RJ020803@users.noreply.github.com>
2024-10-07 20:55:49 +0530

    gallery part done

--- app/gallery/travel/page.jsx
+++ app/gallery/travel/page.jsx
@@ -1,91 +1,93 @@
-import React from 'react'
+import React from 'react';
 
 export default function Travel() {
   return (
     <>
-    <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-  <div className="grid gap-4">
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center"
-        src="https://tse1.mm.bing.net/th?id=OIP.yqo20pxTU3tYyIVmVU89vwHaE8&pid=Api&P=0&h=180"
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center "
-        src="https://tse4.mm.bing.net/th?id=OIP.vwxLEq0JQMgT7iT62NXVLQHaE8&pid=Api&P=0&h=180"
-        alt="gallery-photo"
-      />
-    </div>
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center"
-        src="https://tse2.mm.bing.net/th?id=OIP.ckuMB1vAkHnqoAL2rrRGSAHaE8&pid=Api&P=0&h=180"
-        alt="gallery-photo"
-      />
-    </div>
-  </div>
-  <div className="grid gap-4">
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center"
-        src="https://tse1.mm.bing.net/th?id=OIP.QkFDvHUyk04woTHql7mWiAHaE7&pid=Api&P=0&h=180"
-        alt="gallery-photo"
-      />
-    </div>
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center"
-        src="https://tse1.mm.bing.net/th?id=OIP.XUzzCjfPH5yilgli1hN_ogHaEo&pid=Api&P=0&h=180"
-        alt="gallery-photo"
-      />
-    </div>
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center "
-        src="https://tse2.mm.bing.net/th?id=OIP.5hEOlJa02MCyFQ5_5k1VNwHaEu&pid=Api&P=0&h=180"
-        alt="gallery-photo"
-      />
-    </div>
-  </div>
-  <div className="grid gap-4">
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center"
-        src=""
-        alt="gallery-photo"
-      />
-    </div>
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center "
-        src="https://tse3.mm.bing.net/th?id=OIP.N5T42-v-QfjgDurY7i6TJQHaE8&pid=Api&P=0&h=180"
-        alt="gallery-photo"
-      />
-    </div>
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center"
-        src="https://tse4.mm.bing.net/th?id=OIP.832E9jD0wJX-Y9u4C5JspQHaEo&pid=Api&P=0&h=180"
-        alt="gallery-photo"
-      />
-    </div>
-  </div>
-  <div className="grid gap-4">
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center"
-        src="https://tse2.mm.bing.net/th?id=OIP.3dipwp0Bbq9zMQNwavHc3gHaFj&pid=Api&P=0&h=180"
-        alt="gallery-photo"
-      />
-    </div>
-    <div>
-      <img
-        className="h-auto max-w-full rounded-lg object-cover object-center"
-        src="https://tse4.mm.bing.net/th?id=OIP._If-4fsmLeZcuKe15YahsAHaDx&pid=Api&P=0&h=180"
-        alt="gallery-photo"
-      />
-    </div>
-  </div>
-</div>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+        <div className="grid gap-4">
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse1.mm.bing.net/th?id=OIP.yqo20pxTU3tYyIVmVU89vwHaE8&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse4.mm.bing.net/th?id=OIP.vwxLEq0JQMgT7iT62NXVLQHaE8&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse2.mm.bing.net/th?id=OIP.ckuMB1vAkHnqoAL2rrRGSAHaE8&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse1.mm.bing.net/th?id=OIP.QkFDvHUyk04woTHql7mWiAHaE7&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse1.mm.bing.net/th?id=OIP.XUzzCjfPH5yilgli1hN_ogHaEo&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse2.mm.bing.net/th?id=OIP.5hEOlJa02MCyFQ5_5k1VNwHaEu&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse3.mm.bing.net/th?id=OIP.xlkNoGsbyUCCbONLg5-oGAHaE8&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse3.mm.bing.net/th?id=OIP.N5T42-v-QfjgDurY7i6TJQHaE8&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse4.mm.bing.net/th?id=OIP.832E9jD0wJX-Y9u4C5JspQHaEo&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse2.mm.bing.net/th?id=OIP.3dipwp0Bbq9zMQNwavHc3gHaFj&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+          <div>
+            <img
+              className="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://tse4.mm.bing.net/th?id=OIP._If-4fsmLeZcuKe15YahsAHaDx&pid=Api&P=0&h=180"
+              alt="gallery-photo"
+            />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
